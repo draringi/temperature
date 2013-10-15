41bd0a053dcac7f82074e23d0d513ae4ffc734a3
Michael Williams <draringi@draringi.net>
2013-10-15 19:18:35 -0400

    Added temperature retrieval test

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -12,10 +12,20 @@ public class TemperatureTest {
   // Add multiple tests to check all functions of
   // {@Code Temperature} class.
 
-  // @Test
-  // public void .... (){
-  //    ...
-  // }
+   @Test
+   public void test_getTemperature (){
+      System.out.println("Test Temperature's getValue funtion");
+      Temperature T = new Temperature(0,Temperature.Units.CELSIUS);
+      assertTrue(T.getValue() == 0.0);
+   }
+
+   @Test
+   public void test_toKelvin (){
+      System.out.println("Test Temperature's conversion to Kelvin");
+      Temperature T = new Temperature(0,Temperature.Units.CELSIUS);
+      assertTrue(T.getValue() == 0.0);
+   }
+
 
   // @Test
   // public void .... (){
